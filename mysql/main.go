06904ffc5efc168d411040245c5e2b9f08a6f75f
kdas6141 <65910294+kdas6141@users.noreply.github.com>
2020-07-14 14:45:00 -0700

    Add files via upload

--- mysql/main.go
+++ mysql/main.go
@@ -1,1 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	fmt.Println("Go MySQL Tutorial");
+
+	//Open database connection
+	db, err := sql.Open("mysql", "root:password1@tcp(127.0.0.1:3306)/testdb")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+    fmt.Println("Successfully connected to the database");
+
+    testdb, err := db.Query("USE testdb")
+
+    if err != nil {
+        panic(err.Error())
+    }
+    defer testdb.Close()
+
+    fmt.Println("Successfully switch to the database");
+
+	insert, err := db.Query("INSERT INTO users VALUES('Soumik Das')")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer insert.Close()
+
+	fmt.Println("Successfully inserted into user tables");
+}
 
