a8285f6071aada3936d489179f8a879cf91aaf74
kdas6141 <65910294+kdas6141@users.noreply.github.com>
2020-07-23 09:14:01 -0700

    Add files via upload

---
+++ microservices/handlers/products.go
@@ -0,0 +1,94 @@
+package handlers
+
+import (
+	"fmt"
+	"context"
+	"log"
+	"net/http"
+	"strconv"
+	"microservice/data"
+	"github.com/gorilla/mux"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+    lp := data.GetProducts()
+
+    err := lp.ToJSON(rw)
+    if err != nil {
+        http.Error(rw, "Unable to marshal jason", http.StatusInternalServerError)
+    }
+}
+
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
+    p.l.Println("Handle POST Product")
+
+    prod := r.Context().Value(KeyProduct{}).(data.Product)
+	data.AddProduct(&prod)
+}
+
+func (p Products) UpdateProduct(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
+		return
+	}
+
+	p.l.Println("Handle PUT Product", id)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+
+	err = data.UpdateProduct(id, &prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
+}
+
+type KeyProduct struct{}
+
+func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+
+		err := prod.FromJSON(r.Body)
+		if err != nil {
+			p.l.Println("[ERROR] deserializing product", err)
+			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		// validate the product
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("[ERROR] validating product", err)
+			http.Error(
+				rw,
+				fmt.Sprintf("Error validating product: %s", err),
+				http.StatusBadRequest,
+			)
+			return
+		}
+
+		// add the product to the context
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r2 := r.WithContext(ctx)
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(rw, r2)
+	})
+}
